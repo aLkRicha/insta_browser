65cab28e71cdb8d563b25e4d201be11ccce14027
Aleksej <krich.al.vl@gmail.com>
2017-06-29 14:52:40 +0300

    remove duplicate functionality

--- insta_browser/browser.py
+++ insta_browser/browser.py
@@ -148,10 +148,7 @@ class Browser:
         processed_location = re.sub('^(/?explore/locations/|/|/?locations/)', '', location)
         br.get("https://www.instagram.com/explore/locations/{}".format(processed_location))
         time.sleep(.5)
-        if count:
-            posts_count = self.like_limit - 9 if count > self.like_limit else count
-        else:
-            posts_count = self.like_limit - 9
+        posts_count = self.__get_posts_count(count)
         self.logger.log("Start liking top posts from {} location".format(processed_location))
         processor = NotFeedProcessor(br, self.logger)
         processor.like_top()
@@ -163,13 +160,16 @@ class Browser:
         br = self.browser
         br.get("https://www.instagram.com/explore/tags/{}".format(tag))
         time.sleep(.5)
-        if count:
-            posts_count = self.like_limit - 9 if count > self.like_limit else count
-        else:
-            posts_count = self.like_limit - 9
+        posts_count = self.__get_posts_count(count)
         self.logger.log("Start liking top posts from {} location".format(tag))
         processor = NotFeedProcessor(br, self.logger)
         processor.like_top()
         result = processor.like_latest(posts_count)
         self.liked = result['liked']
         self.skipped = result['skipped']
+
+    def __get_posts_count(self, count):
+        if count:
+            return self.like_limit - 9 if count > self.like_limit else count
+        else:
+            return self.like_limit - 9
